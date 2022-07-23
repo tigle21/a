dcb56ba9b894236c9f4498b3e383e446a46ad26c
tigle21 <109816477+tigle21@users.noreply.github.com>
2022-07-23 20:43:48 +0530

    a

---
+++ Music Player/js/music-list.js
@@ -0,0 +1,39 @@
+let allMusic = [
+  {
+    name: "JAY SHREE RAM",
+    artist: " ",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "JAY SHREE RAM",
+    artist: " ",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "JAY SHREE RAM",
+    artist: " ",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "JAY SHREE RAM",
+    artist: " ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "JAY SHREE RAM",
+    artist: " ",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "JAY SHREE RAM",
+    artist: " ",
+    img: "music-6",
+    src: "music-6"
+  },
+
+];
